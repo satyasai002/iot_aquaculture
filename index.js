a91e79237ebc5509dc10a694541fdf9a3bc35bbc
satyasai002 <satyasaimadarapu@gmai.com>
2023-03-02 00:13:16 +0530

    added sockets

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const http = require("http");
 const mongoose = require("mongoose");
+const { Server } = require("socket.io");
 
 const app = express();
 
 const apiRoutes = require("./routes/api");
-const userRoutes = require("./routes/user")
+const userRoutes = require("./routes/user");
 
 app.use(cors());
 app.use(morgan("tiny"));
@@ -20,9 +22,59 @@ mongoose.Promise = global.Promise;
 app.use("/api", apiRoutes);
 app.use("/user", userRoutes);
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3001","http://localhost:3002"],
+    methods: ["GET", "POST"],
+  },
+});
+
+let users = [];
+let boats = [];
+
+const addUser = (userId,boatId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId,boatId, socketId });
+};
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+const getUser = (boatId) => {
+  return users.filter((user) => user.boatId === boatId);
+};
+
+io.on("connection", (socket) => {
+  //when ceonnect
+
+  //take userId and socketId from user
+  socket.on("addUser", (userId,boatId) => {
+    addUser(userId,boatId, socket.id);
+    console.log("added user")
+  });
+
+  //send and get message
+  socket.on("sendData",({ boatId, Data }) => {
+    const users1 =getUser(boatId);
+    console.log("a data is been sent")
+    console.log(users1)
+    users1.map((user)=>{
+       io.to(user.socketId).emit("getData",  Data);
+    })
+    
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server started on PORT : ${PORT}`);
 });
