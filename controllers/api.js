231f39c3ebdb9d0464678df1172d164f7b6d2180
satyasai002 <satyasaimadarapu@gmai.com>
2023-04-12 23:28:22 +0530

    temp code for iot

--- controllers/api.js
+++ controllers/api.js
@@ -1,114 +1,160 @@
 const mongoose = require("mongoose");
 const Boat = require("../models/Boat");
-const Reading = require("../models/Readings")
+const Reading = require("../models/Readings");
 // const moment = require("moment")
 const moment = require("moment-timezone");
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
+const text = "We are writing to report a smoke detection at VIT. Our smoke detectors were triggered, and we believe that there may be a fire on the premises.The safety of our occupants and property is our top priority, and we request your immediate assistance in addressing this situation."
 
 exports.api_add_data = async (req, res) => {
-    const {boatId,temp,tds,turbidity} = req.body;
-    const Data = {
-      Temp:temp,
-      Tds:tds,
-      Turb:turbidity,
-    }
-    // req.app.get("io").emit("senddata", boatId);
-    // console.log(req.app.get('users'))
-    const users1 = req.app.get("getUser")(boatId);
-    console.log(user1)
-    await users1.map((user) => {
-      req.app.get("io").to(user.socketId).emit("getData", Data);
-      console.log("emited data to ",user.userId)
-    });
-    var now = new Date();
-    // var startOfToday = new Date(
-    //   now.getFullYear(),
-    //   now.getMonth(),
-    //   now.getDate(),
-    // );
-    // console.log(moment().format("MMM Do YY"));
-    var moment = require("moment-timezone");
-    console.log(moment().tz("Asia/Kolkata").format("MMM Do YY"));
+  const { boatId, temp, tds, turbidity } = req.body;
+  const Data = {
+    Temp: temp,
+    Tds: tds,
+    Turb: turbidity,
+  };
+  const mailgun = new Mailgun(formData);
+  const mg = mailgun.client({
+    username: "api",
+    key: "d2cb23842767724e401639d95bad2bb3-2cc48b29-ae2a22b2",
+  });
+  if(temp==108){
 
-    const data = await Reading.findOne(
-      { boatId: req.body.boatId, created:  moment().tz("Asia/Kolkata").format("MMM Do YY") },
-      {}
-    );
-    if(data!==null){
-      const ncount = +data.count+1;
-      const ntemp = String(+data.temp + (+req.body.temp - +data.temp)/ncount);
-      const nturbidity =
-        String(+data.turbidity + (+req.body.turbidity - +data.turbidity) / ncount);
-      const ntds = String(+data.tds + (+req.body.tds - +data.tds) / ncount);
-      const nData = {
-        temp: ntemp,
-        turbidity: nturbidity,
-        tds: ntds,
-        count: ncount,
-      };
-      let doc = await Reading.findOneAndUpdate(
-        {
-          boatId: req.body.boatId,
-          created:  moment().tz("Asia/Kolkata").format("MMM Do YY"),
-        },
-        nData,
-        {
-          new: true,
-        }
-      );
-      res.status(201).json(doc);
-    }
-    else{
-      const reading = new Reading({
-        _id: new mongoose.Types.ObjectId(),
-        boatId: req.body.boatId,
-        temp: req.body.temp,
-        turbidity: req.body.turbidity,
-        tds: req.body.tds,
-        count: 1,
-        created: moment().tz("Asia/Kolkata").format("MMM Do YY"),
-      });
-      await reading
-        .save()
-        .then((result) => {
-          res.status(201).json({ message: "Added Reading successfully" });
-        })
-        .catch((err) => {
-          res.status(500).json({
-            error: err,
-          });
-        });
-    }
-    
-}
-exports.api_add_boat = (req, res) => {
-        const boat = new Boat({
-          _id: new mongoose.Types.ObjectId(),
-          name: req.body.name,
-        });
-        boat
-    .save()
-    .then((result) => {
-      console.log(result);
-      res.status(201).json({message: "Created boat successfully"})}).catch((err)=>{
-        console.log(err);
-      res.status(500).json({
-        error: err,
-      });
+    await mg.messages
+      .create("sandbox023d21309fe94e5a8412b14d96bc3ac2.mailgun.org", {
+        from: "Fire alarm system <mailgun@sandbox023d21309fe94e5a8412b14d96bc3ac2.mailgun.org>",
+        to: ["satyasaimadarapu@gmail.com"],
+        subject: "Both smoke and flame detected",
+        text: "We are writing to report a smoke detection at VIT. Our smoke detectors were triggered",
+        html: "<h1 style='color: red'>Both smoke and flame detected!</h1>",
       })
-};
+      .then((msg) => console.log(msg)) // logs response data
+      .catch((err) => console.log(err)); // logs any error
+  }
+  if (temp == 100) {
+    await mg.messages
+      .create("sandbox023d21309fe94e5a8412b14d96bc3ac2.mailgun.org", {
+        from: "Fire alarm system <mailgun@sandbox023d21309fe94e5a8412b14d96bc3ac2.mailgun.org>",
+        to: ["satyasaimadarapu@gmail.com"],
+        subject: "Flame detected",
+        text: "",
+           html: "<h1 style='color: red'>Flame detected!</h1>",
+      })
+      .then((msg) => console.log(msg)) // logs response data
+      .catch((err) => console.log(err)); // logs any error
+  }
+  if (temp == 69) {
+    await mg.messages
+      .create("sandbox023d21309fe94e5a8412b14d96bc3ac2.mailgun.org", {
+        from: "Fire alarm system <mailgun@sandbox023d21309fe94e5a8412b14d96bc3ac2.mailgun.org>",
+        to: ["satyasaimadarapu@gmail.com"],
+        subject: "Smoke detected",
+        text: "",
+            html: "<h1 style='color: red'>Smoke detected!</h1>",
+      })
+      .then((msg) => console.log(msg)) // logs response data
+      .catch((err) => console.log(err)); // logs any error
+  }
+  // req.app.get("io").emit("senddata", boatId);
+  // console.log(req.app.get('users'))
+  const users1 = req.app.get("getUser")(boatId);
 
-exports.api_get_boats = (req, res) => {
-    try{
-    Boat.findAll({}, function (docs) {
-      res.status(201).json(docs);
-    })}
-    catch(err){
-        console.log(err)
+  await users1.map((user) => {
+    req.app.get("io").to(user.socketId).emit("getData", Data);
+    console.log("emited data to ", user.userId);
+  });
+  var now = new Date();
+  // var startOfToday = new Date(
+  //   now.getFullYear(),
+  //   now.getMonth(),
+  //   now.getDate(),
+  // );
+  // console.log(moment().format("MMM Do YY"));
+  var moment = require("moment-timezone");
+  console.log(moment().tz("Asia/Kolkata").format("MMM Do YY"));
+
+  const data = await Reading.findOne(
+    {
+      boatId: req.body.boatId,
+      created: moment().tz("Asia/Kolkata").format("MMM Do YY"),
+    },
+    {}
+  );
+  if (data !== null) {
+    const ncount = +data.count + 1;
+    const ntemp = String(+data.temp + (+req.body.temp - +data.temp) / ncount);
+    const nturbidity = String(
+      +data.turbidity + (+req.body.turbidity - +data.turbidity) / ncount
+    );
+    const ntds = String(+data.tds + (+req.body.tds - +data.tds) / ncount);
+    const nData = {
+      temp: ntemp,
+      turbidity: nturbidity,
+      tds: ntds,
+      count: ncount,
+    };
+    let doc = await Reading.findOneAndUpdate(
+      {
+        boatId: req.body.boatId,
+        created: moment().tz("Asia/Kolkata").format("MMM Do YY"),
+      },
+      nData,
+      {
+        new: true,
+      }
+    );
+    res.status(201).json(doc);
+  } else {
+    const reading = new Reading({
+      _id: new mongoose.Types.ObjectId(),
+      boatId: req.body.boatId,
+      temp: req.body.temp,
+      turbidity: req.body.turbidity,
+      tds: req.body.tds,
+      count: 1,
+      created: moment().tz("Asia/Kolkata").format("MMM Do YY"),
+    });
+    await reading
+      .save()
+      .then((result) => {
+        res.status(201).json({ message: "Added Reading successfully" });
+      })
+      .catch((err) => {
         res.status(500).json({
           error: err,
         });
-    }
+      });
+  }
+};
+exports.api_add_boat = (req, res) => {
+  const boat = new Boat({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+  });
+  boat
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({ message: "Created boat successfully" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
 
-
-
-}
+exports.api_get_boats = (req, res) => {
+  try {
+    Boat.findAll({}, function (docs) {
+      res.status(201).json(docs);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: err,
+    });
+  }
+};
